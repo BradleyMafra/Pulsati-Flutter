f1e972655ce40cf9f9dc09e5d8e2339cc136eeaa
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-03-29 16:26:19 -0300

    Update

--- financas/lib/pages/entradas.dart
+++ financas/lib/pages/entradas.dart
@@ -1,5 +1,10 @@
-import 'package:financas/pages/home.dart';
+// ignore_for_file: unnecessary_string_interpolations
+
 import 'package:flutter/material.dart';
+
+String nome = '';
+String valor = '';
+String data = '';
 
 class EntradasPage extends StatelessWidget {
   const EntradasPage({super.key});
@@ -21,9 +26,115 @@ class EntradasPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text('Entradas'),
+          Row(
+            children: [
+              const SizedBox(width: 100),
+              TextButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) => Dialog(
+                      child: SizedBox(
+                        height: 400,
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 30, top: 40, right: 30),
+                              child: TextField(
+                                onChanged: (String value) {
+                                  nome = value;
+                                },
+                                decoration: const InputDecoration(
+                                  hintText: 'Nome',
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 30, top: 40, right: 30),
+                              child: TextField(
+                                onChanged: (String value) {
+                                  valor = value;
+                                },
+                                decoration: const InputDecoration(
+                                  hintText: 'Valor',
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 30, top: 40, right: 30),
+                              child: TextField(
+                                onChanged: (String value) {
+                                  data = value;
+                                },
+                                decoration: const InputDecoration(
+                                  hintText: 'Data',
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 60),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('Salvar'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                child: const Text(
+                  '+ Nova entrada',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 182, 252, 219),
+                    fontSize: 26,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Text(
+                    '$nome',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    '$valor',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    '$data',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
+      backgroundColor: const Color.fromARGB(255, 176, 13, 252),
     );
   }
 }
